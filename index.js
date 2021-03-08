be28a2b724633aa12017057079f5ce73818b2731
Heather Smith <halexsmith86@gmail.com>
2021-03-08 15:02:55 -0500

    Added inquirer prompts for each category

--- index.js
+++ index.js
@@ -5,38 +5,118 @@ const cTable = require('console.table');
 
 // connecting the database
 const connection = mysql.createConnection({
-  host: 'localhost',
+    host: 'localhost',
 
-  // setting a port
-  port: process.env.PORT || 3306,
+    // setting a port
+    port: process.env.PORT || 3306,
 
-  // username
-  user: 'root',
+    // username
+    user: 'root',
 
-  // password
-  password: 'toby0000',
+    // password
+    password: 'toby0000',
 
-  // Change this database name!
-  database: 'greatBay_DB',
+    // Change this database name!
+    database: 'greatBay_DB',
 });
 
-// inquirer prompts that
+// inquirer prompts main menu
 const start = () => {
     inquirer
-      .prompt({
-        name: 'postOrBid',
-        type: 'list',
-        message: 'Would you like to [POST] an auction or [BID] on an auction?',
-        choices: ['POST', 'BID', 'EXIT'],
-      })
-      .then((answer) => {
-        // based on their answer, either call the bid or the post functions
-        if (answer.postOrBid === 'POST') {
-          postAuction();
-        } else if (answer.postOrBid === 'BID') {
-          bidAuction();
-        } else {
-          connection.end();
+        .prompt({
+            name: 'which',
+            type: 'list',
+            message: 'Welcome! Would you like to add a [DEPARTMENT], a [ROLE], or an [EMPLOYEE]?',
+            choices: ['DEPARTMENT', 'ROLE', 'EMPLOYEE', 'EXIT'],
+        })
+        .then((response) => {
+            // based on their answer, either call the bid or the post functions
+            if (response.which === 'DEPARTMENT') {
+                addDept();
+            } else if (response.which === 'ROLE') {
+                addRole();
+            } else if (response.which === 'EMPLOYEE') {
+                addEmp();
+            }
+            else {
+                connection.end();
+            }
+        });
+};
+
+// prompts for adding a department
+const addDept = () => {
+    inquirer
+        .prompt({
+            name: 'deptid',
+            type: 'input',
+            message: 'What is the department\'s ID number?',
+        },
+        {
+            name: 'deptname',
+            type: 'input',
+            message: 'What is the department\'s name?',
         }
-      });
-  };
+        )
+        .then((response) => {
+            // input needs to be added to the db
+        });
+};
+
+// prompts for adding a role
+const addRole = () => {
+    inquirer
+        .prompt({
+            name: 'roleid',
+            type: 'input',
+            message: 'What is the role\'s ID number?',
+        },
+        {
+            name: 'rolename',
+            type: 'input',
+            message: 'What is the role\'s name?',
+        },
+        {
+            name: 'rolesal',
+            type: 'input',
+            message: 'What is the role\'s yearly salary?',
+        },
+        {   name: 'roledept',
+            type: 'input',
+            message: 'What is the department of the role\'s ID number?',
+    })
+        .then((response) => {
+            // input needs to be added to the db
+        });
+}
+
+// prompts for adding an employee
+const addEmp = () => {
+    inquirer
+        .prompt({
+            name: 'empid',
+            type: 'input',
+            message: 'What is the employee\'s ID number?',
+        },
+        {
+            name: 'firstname',
+            type: 'input',
+            message: 'What is the employee\'s first name?',
+        },
+        {
+            name: 'lastname',
+            type: 'input',
+            message: 'What is the employee\'s last name?',
+        },
+        {   name: 'erid',
+            type: 'input',
+            message: 'What is the department of the employee\'s ID number?',
+        },
+        {
+            name: 'manaid',
+            type: 'input',
+            message: 'What is the employee\'s manager\'s ID number?',
+        }).then((response) => {
+            // input needs to be added to the db
+        });
+}
