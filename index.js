3a38e0a49d62155849d732e71ab7464bfbed0055
Heather Smith <halexsmith86@gmail.com>
2021-03-08 13:59:27 -0500

    Added console.table to dependencies

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // dependencies
 const mysql = require('mysql');
 const inquirer = require('inquirer');
+const cTable = require('console.table');
 
 // connecting the database
 const connection = mysql.createConnection({
@@ -18,3 +19,24 @@ const connection = mysql.createConnection({
   // Change this database name!
   database: 'greatBay_DB',
 });
+
+// inquirer prompts that
+const start = () => {
+    inquirer
+      .prompt({
+        name: 'postOrBid',
+        type: 'list',
+        message: 'Would you like to [POST] an auction or [BID] on an auction?',
+        choices: ['POST', 'BID', 'EXIT'],
+      })
+      .then((answer) => {
+        // based on their answer, either call the bid or the post functions
+        if (answer.postOrBid === 'POST') {
+          postAuction();
+        } else if (answer.postOrBid === 'BID') {
+          bidAuction();
+        } else {
+          connection.end();
+        }
+      });
+  };
